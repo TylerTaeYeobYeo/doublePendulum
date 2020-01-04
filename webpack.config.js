743449960390b0f00e5a9ea1abf24a7124cd73ec
TylerTaeYeobYeo <id1423fo@gmail.com>
2020-01-04 22:15:44 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        hot: true,
+        inline: true,
+        host: '0.0.0.0',
+        port: 4000,
+        contentBase: __dirname + '/public/',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
